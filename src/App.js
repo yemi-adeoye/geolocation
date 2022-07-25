d56936aec50e8eee355a7f53cc016c0df5410edb
Adeyemi Adeoye <adeyemi_adeoye@hotmail.com>
2022-07-25 04:41:51 -0500

    adds map props

--- src/App.js
+++ src/App.js
@@ -1,46 +1,36 @@
-import "./App.css";
-import InputLAbelError from "./components/InputLAbelError";
-import { useState,  } from "react";
-import Map from "./components/Map";
-import axios from "axios";
+import './App.css';
+import InputLAbelError from './components/InputLAbelError';
+import { useState } from 'react';
+import Map from './components/Map';
+import axios from 'axios';
 
 function App() {
-  const ACCESS_TOKEN = 'pk.eyJ1IjoieWhlbW1pIiwiYSI6ImNrb2dtajAxejBldjkydXA3ZnBvejlmYmEifQ.lqixKQsslg4DnVlG4TWDNQ'
+  const ACCESS_TOKEN =
+    'pk.eyJ1IjoieWhlbW1pIiwiYSI6ImNrb2dtajAxejBldjkydXA3ZnBvejlmYmEifQ.lqixKQsslg4DnVlG4TWDNQ';
 
-  const inititalAddress = { from: "", to: "" };
+  const inititalAddress = { from: '', to: '' };
   const [address, setAddress] = useState(inititalAddress);
 
   const onAdressChange = (e) => {
     setAddress({ ...address, [e.target.id]: e.target.value });
   };
 
-  const getRoute = async (e) => {
-    console.log("...getting route");
-    const from = [-84.518641, 39.13427];
-    const to = [-84.512023, 39.102779];
-    const urlConst = 'https://api.mapbox.com/directions/v5/mapbox/driving-traffic/';
-    const urlVar = `${from[0]},${from[1]};${to[0]},${to[1]}?geometries=geojson&access_token=${ACCESS_TOKEN}`;
-    console.log(urlConst + urlVar)
-    const response = await axios.get(urlConst + urlVar);
-    console.log(response)
-  };
-
   return (
-    <div className="App">
+    <div className='App'>
       <InputLAbelError
-        id="from"
-        label="From"
+        id='from'
+        label='From'
         value={address.from}
         onTextChange={onAdressChange}
       />
       <InputLAbelError
-        id="to"
-        label="To"
+        id='to'
+        label='To'
         value={address.to}
         onTextChange={onAdressChange}
       />
       <Map />
-      <input type="button" onClick={getRoute} value="Go" />
+      <input type='button' onClick={getRoute} value='Go' />
     </div>
   );
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,32 +1,97 @@
-import {useState, useRef, useEffect} from 'react'
+import { useState, useRef, useEffect } from 'react';
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import mapboxgl from '!mapbox-gl'
-mapboxgl.accessToken = 'pk.eyJ1IjoieWhlbW1pIiwiYSI6ImNrb2dtajAxejBldjkydXA3ZnBvejlmYmEifQ.lqixKQsslg4DnVlG4TWDNQ'
+import mapboxgl from '!mapbox-gl';
 
+mapboxgl.accessToken =
+  'pk.eyJ1IjoieWhlbW1pIiwiYSI6ImNrb2dtajAxejBldjkydXA3ZnBvejlmYmEifQ.lqixKQsslg4DnVlG4TWDNQ';
 
 const Map = () => {
-    useEffect(()=>{
-        initMap();
-    }, []);
+  const container = useRef(null);
+  const map = useRef(null);
 
-    const [longLat, setLongLat] = useState({
-        long: -70.9,
-        lat: 43.35
-    });
-    const [zoom, setZoom] = useState(9);
+  useEffect(() => {
+    initMap();
+  }, []);
 
-    const initMap = () => {
-        const options = {
-            container: '',
-            style: 'mapbox://styles/mapbox/streets-v11',
-            centre: [{...longLat}],
-            zoom
-        }
-        const map = new mapboxgl.Map(options)
+  const [longLat, setLongLat] = useState({
+    long: -122.662323,
+    lat: 45.523751,
+  });
+
+  const [zoom, setZoom] = useState(10);
+
+  const initMap = () => {
+    const options = {
+      container: container.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      centre: [longLat.long, longLat.lat],
+      zoom,
+    };
+
+    map.current = new mapboxgl.Map(options);
+
+    const bounds = [
+      [-123.069003, 45.395273],
+      [-122.303707, 45.612333],
+    ];
+
+    map.current.setMaxBounds(bounds);
+  };
+
+  const getRoute = async (e) => {
+    console.log('...getting route');
+    const from = [-84.518641, 39.13427];
+    const to = [-84.512023, 39.102779];
+    const urlConst =
+      'https://api.mapbox.com/directions/v5/mapbox/driving-traffic/';
+    const urlVar = `${from[0]},${from[1]};${to[0]},${to[1]}?geometries=geojson&access_token=${ACCESS_TOKEN}`;
+    console.log(urlConst + urlVar);
+    const response = await axios.get(urlConst + urlVar);
+    const json = await response.json();
+    const data = json.routes[0];
+    const route = data.geometry.coordinate;
+    const geojson = {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'LineString',
+        coordinates: route,
+      },
+    };
+
+    // if the route already exists on the map, we'll reset it using setData
+    if (map.getSource('route')) {
+      map.getSource('route').setData(geojson);
     }
-  return (
-    <div id="map"></div>
-  )
-}
+    // otherwise, we'll make a new request
+    else {
+      map.addLayer({
+        id: 'route',
+        type: 'line',
+        source: {
+          type: 'geojson',
+          data: geojson,
+        },
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round',
+        },
+        paint: {
+          'line-color': '#3887be',
+          'line-width': 5,
+          'line-opacity': 0.75,
+        },
+      });
+    }
+    // add turn instructions here at the end
 
-export default Map
+    console.log(response);
+  };
+  return (
+    <div>
+      <div ref={container} id='map' style={{ height: '400px' }} />
+    </div>
+  );
+};
+
+export default Map;
